984e87df4a656c11890261765cc7434417dcaede
Elminson De Oleo Baez <edeoleo@gmail.com>
2019-06-17 17:10:21 +0000

    Adding the project

---
+++ database/factories/TaskFactory.php
@@ -0,0 +1,10 @@
+<?php
+use Faker\Generator as Faker;
+$factory->define(App\Task::class, function (Faker $faker) {
+	$users = App\User::pluck('id')->toArray();
+	return [
+		'name' => $faker->unique()->name,
+		'description' => $faker->text,
+		'user_id' => $faker->randomElement($users)
+		];
+});
